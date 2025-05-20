de7080b4e52729444ff5fdf4b428f12b6206f99a
JongHyeon Park <jonghp1128@naver.com>
2025-05-20 14:10:31 +0900

    BST 해결

--- src/main/BinaryTree.java
+++ src/main/BinaryTree.java
@@ -37,7 +37,7 @@ public class BinaryTree<Key extends Comparable<Key>> {
         if (node1.getItem().compareTo(node2.getItem()) != 0) return false;
         // TODO 반환값을 올바르게 작성하시오.
         // 두 트리가 같은지 보는 것
-        return isEqual(node1.getLeft(), node2.getLeft()) && isEqual(node2.getRight(), node2.getRight());
+        return isEqual(node1.getLeft(), node2.getLeft()) && isEqual(node2.getRight(), node1.getRight());
     }
 
     public boolean isEmpty() {
